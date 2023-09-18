39284df4c2f54d7b8b167037eba274d98b0df8b3
Ubah Kahie <ubahkahie@outlook.com>
2023-09-18 16:34:15 +0100

    ticket3unresolved

--- ticket-3/index.js
+++ ticket-3/index.js
@@ -3,10 +3,24 @@ function countNumberOfNinesInNumber(number) {
 
   for (let i = 0; i < number.length; i++) {
     const digit = number[i];
-    if ("9" === digit) {
+    if (9 === digit) {
       count++;
     }
+    return count;
   }
-
-  return count;
 }
+countNumberOfNinesInNumber();
+console.log(countNumberOfNinesInNumber(99.99));
+
+// function countNumberOfNinesInNumber(number) {
+//   let count = 0;
+
+//   for (let i = 0; i < number.length; i++) {
+//     const digit = number[i];
+//     if (9 === digit) {
+//       count++;
+//     }
+//   }
+
+//   return count;
+// }
