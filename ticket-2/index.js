97a96e5525d6c7dfa7b8bec6631c068805dbb632
maramar0414 <maramar0414@gmail.com>
2023-09-18 15:42:10 +0100

    tickets 2

--- ticket-2/index.js
+++ ticket-2/index.js
@@ -1,7 +1,7 @@
 function extractInitialsFromFullName(fullName) {
 /** Assumes that each name within the full name is separated by a "space" that can be used as a separator/delimiter.  */
 const names = fullName.split();
-let initials = ()
+let initials = "";
 }
 
 for (let i = 0; i < names.length; i++) {
